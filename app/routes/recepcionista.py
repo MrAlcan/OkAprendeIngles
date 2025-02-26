c8a3c04349b0a240c8d85912058d51b6356b4bcf
Zaceli <zzacceloayza@gmail.com>
2025-02-26 17:54:58 -0400

    recepcionista/formulario

--- app/routes/recepcionista.py
+++ app/routes/recepcionista.py
@@ -13,3 +13,12 @@ def vista_inicio(datos_usuario):
     primer_nombre = nombres.split(' ')[0]
     primer_apellido = apellidos.split(' ')[0]
     return render_template('recepcionista/inicio.html', primer_nombre = primer_nombre, primer_apellido = primer_apellido)
+
+@recepcionista_bp.route('/formulario', methods=['GET'])
+#@token_requerido
+def vista_crear_usuario():
+    #nombres = str(datos_usuario['primer_nombre'])
+    #apellidos = str(datos_usuario['primer_apellido'])
+    primer_nombre = "za"
+    primer_apellido = "za"
+    return render_template('recepcionista/formu.html', primer_nombre = primer_nombre, primer_apellido = primer_apellido)
